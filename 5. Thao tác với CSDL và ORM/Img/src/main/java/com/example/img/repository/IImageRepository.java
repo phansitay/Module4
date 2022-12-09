a7103ac15ce79d56d984d946fcde2f300192174c
phansitay <82353942+phansitay@users.noreply.github.com>
2022-12-09 20:30:27 +0700

    5

--- 5. Thao tác với CSDL và ORM/Img/src/main/java/com/example/img/repository/IImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.img.repository;
-
-import com.example.img.model.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IImageRepository extends JpaRepository<Image,Integer> {
-}
